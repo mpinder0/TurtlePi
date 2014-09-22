5679209f2f91ce36bc2c71483c3bc20a8bb180a3
mpinder <matthew.pinder@autotech.co.uk>
2014-09-22 19:29:24 +0100

    updates to api to GET Points and GET or POST PointValues

--- Historian/api.py
+++ Historian/api.py
@@ -1,3 +1,85 @@
 """
 Define REST API routes and methods for flask to handle
 """
+
+from datetime import timedelta
+
+from flask import jsonify
+from flask import request
+
+from app import app
+from models import Point, PointValue
+from utils import *
+
+
+
+@app.route('/api/point/<string:point_name>', methods=['GET'])
+def get_point(point_name):
+    obj = get_object_or_404(Point, Point.name == point_name)
+    dictionary = get_dictionary_from_model(obj)
+    return jsonify(dictionary)
+
+
+@app.route('/api/point_value/<string:point_name>', methods=['GET'])
+def get_point_value(point_name):
+    query_from = request.args.get("from")
+    query_to = request.args.get("to")
+    query_at = request.args.get("at")
+
+    if (query_from is not None) | (query_to is not None):
+        # if "from" or "to" timestamps are present in the query string, get values from that time span.
+        results = get_values_between(point_name, query_from, query_to)
+    elif query_at is not None:
+        # if "at" timestamp is present in the query string, get a single value.
+        results = get_value_at(point_name, query_at)
+    else:
+        results = get_all_values(point_name)
+
+    if not results:
+        # no objects found matching the request - 404
+        abort(404)
+    return jsonify(results)
+
+
+def get_all_values(point_name):
+    query = PointValue.select().join(Point).where(Point.name == point_name)
+    return get_dictionary_from_query(query)
+
+
+def get_values_between(point_name, query_from, query_to):
+    try:
+        timestamp_from = DatetimeConverter.to_python(str(query_from))
+        timestamp_to = DatetimeConverter.to_python(str(query_to))
+    except ValueError:
+        abort(400)
+
+    query = PointValue.select().join(Point)\
+        .where(Point.name == point_name)\
+        .where(PointValue.timestamp.between(timestamp_from, timestamp_to))
+
+    return get_dictionary_from_query(query)
+
+
+def get_value_at(point_name, query_at):
+    try:
+        timestamp_at = DatetimeConverter.to_python(str(query_at))
+    except ValueError:
+        abort(400)
+
+    span = timedelta(microseconds=999999)
+    query = PointValue.select().join(Point)\
+        .where(Point.name == point_name)\
+        .where(PointValue.timestamp.between(timestamp_at, timestamp_at + span))\
+        .limit(1)
+
+    return get_dictionary_from_query(query)
+
+
+@app.route('/api/point_value/<string:point_name>/<float:value>', methods=['POST'])
+def set_point_value(point_name, value):
+    try:
+        point = Point.get(Point.name == point_name)
+    except DoesNotExist:
+        abort(404)
+    new_value = PointValue.create(point_id=point, value=value)
+    return jsonify(get_dictionary_from_model(new_value))
---
+++ Historian/utils.py
@@ -0,0 +1,84 @@
+from peewee import Model
+from peewee import DoesNotExist
+from peewee import SelectQuery
+from peewee import ForeignKeyField
+from flask import abort
+from datetime import datetime
+
+def get_object_or_404(query_or_model, *query):
+    if not isinstance(query_or_model, SelectQuery):
+        query_or_model = query_or_model.select()
+    try:
+        return query_or_model.where(*query).get()
+    except DoesNotExist:
+        abort(404)
+
+
+def get_dictionary_from_model(model, fields=None, exclude=None):
+    model_class = type(model)
+    data = {}
+
+    fields = fields or {}
+    exclude = exclude or {}
+    curr_exclude = exclude.get(model_class, [])
+    curr_fields = fields.get(model_class, model._meta.get_field_names())
+
+    for field_name in curr_fields:
+        if field_name in curr_exclude:
+            continue
+        field_obj = model_class._meta.fields[field_name]
+        field_data = model._data.get(field_name)
+        if isinstance(field_obj, ForeignKeyField) and field_data and field_obj.rel_model in fields:
+            rel_obj = getattr(model, field_name)
+            data[field_name] = get_dictionary_from_model(rel_obj, fields, exclude)
+        else:
+            data[field_name] = field_data
+    return data
+
+
+def get_dictionary_from_query(query):
+    dictionary = {}
+    for i, value in enumerate(query):
+        dictionary[i] = get_dictionary_from_model(value)
+    return dictionary
+
+
+def get_model_from_dictionary(model, field_dict):
+    if isinstance(model, Model):
+        model_instance = model
+        check_fks = True
+    else:
+        model_instance = model()
+        check_fks = False
+    models = [model_instance]
+    for field_name, value in field_dict.items():
+        field_obj = model._meta.fields[field_name]
+        if isinstance(value, dict):
+            rel_obj = field_obj.rel_model
+            if check_fks:
+                try:
+                    rel_obj = getattr(model, field_name)
+                except field_obj.rel_model.DoesNotExist:
+                    pass
+                if rel_obj is None:
+                    rel_obj = field_obj.rel_model
+            rel_inst, rel_models = get_model_from_dictionary(rel_obj, value)
+            models.extend(rel_models)
+            setattr(model_instance, field_name, rel_inst)
+        else:
+            setattr(model_instance, field_name, field_obj.python_value(value))
+    return model_instance, models
+
+
+class DatetimeConverter(object):
+    date_format = '%Y-%m-%d'
+    time_format = '%H:%M:%S'
+    datetime_format = ' '.join([date_format, time_format])
+
+    @classmethod
+    def to_python(cls, value):
+        return datetime.strptime(value, cls.datetime_format)
+
+    @classmethod
+    def to_url(cls, value):
+        return value.strftime(cls.datetime_format)
